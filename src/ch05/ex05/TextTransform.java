da1336b16fbe21b10d35a699340159b1dc5045c4
bjohns62 <blaine.johnson@optum.com>
2020-04-19 13:00:46 -0600

    add ch05.ex06

--- src/ch05/ex05/TextTransform.java
+++ src/ch05/ex05/TextTransform.java
@@ -7,27 +7,32 @@ import java.util.Scanner;
 
 public class TextTransform {
   public static void toLowerCase(Path inPath, Path outPath) {
+    Scanner in = null;
+    PrintWriter out = null;
     try {
-      Scanner in = null;
-      PrintWriter out = null;
-      try {
-        in = new Scanner(inPath);
-        out = new PrintWriter(outPath.toFile());
-        while (in.hasNext()) {
-          out.println(in.next().toLowerCase());
-        }
-      } finally {
-        if (in != null) {
-          in.close();
-        }
-        if (out != null) {
-          out.close();
-        }
+      in = new Scanner(inPath);
+      out = new PrintWriter(outPath.toFile());
+      while (in.hasNext()) {
+        out.println(in.next().toLowerCase());
       }
     } catch (IOException e) {
       e.printStackTrace();
     } catch (IllegalStateException e) {
       e.printStackTrace();
+    }
+    if (in != null) {
+      try {
+        in.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    if (out != null) {
+      try {
+        out.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
   }
 }
---
+++ test/ch05/ex06/TextTransformTest.java
@@ -0,0 +1,67 @@
+package ch05.ex06;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class TextTransformTest {
+  private PrintStream originalErr;
+  private ByteArrayOutputStream err;
+
+  @BeforeEach
+  public void setup() {
+    originalErr = System.err;
+    err = new ByteArrayOutputStream();
+    System.setErr(new PrintStream(err));
+  }
+
+  @AfterEach
+  public void teardown() {
+    System.setErr(originalErr);
+    err = null;
+  }
+
+  @Test
+  public void testToLowerCase() throws IOException {
+    Path in = Files.createTempFile("in", ".txt");
+    BufferedWriter bw = new BufferedWriter(new FileWriter(in.toFile()));
+    bw.write("FoO");
+    bw.newLine();
+    bw.write("bAr");
+    bw.newLine();
+    bw.write("baz");
+    bw.close();
+
+    Path out = Files.createTempFile("out", ".txt");
+
+    TextTransform.toLowerCase(in, out);
+
+    Scanner outScanner = new Scanner(out);
+    assertEquals("foo", outScanner.next());
+    assertEquals("bar", outScanner.next());
+    assertEquals("baz", outScanner.next());
+    outScanner.close();
+  }
+
+  @Test
+  void testToLowerCaseInvalidInputPaths() throws IOException {
+    Path in = Paths.get("foo.bar");
+    Path out = Files.createTempFile("out", ".txt");
+
+    TextTransform.toLowerCase(in, out);
+    assertTrue(err.toString().contains("NoSuchFileException: foo.bar"));
+  }
+
+}
