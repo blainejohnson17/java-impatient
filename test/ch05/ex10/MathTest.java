940da3b8035d0a5ac2b3c0aa028c2ce68c432896
bjohns62 <blaine.johnson@optum.com>
2020-04-19 14:35:31 -0600

    add tests for ch05.ex10

--- test/ch05/ex10/MathTest.java
+++ test/ch05/ex10/MathTest.java
@@ -7,10 +7,10 @@ class MathTest {
 
   @Test
   void testFactorial() {
-    // assertEquals(1, Math.factorial(0));
-    // assertEquals(1, Math.factorial(1));
-    // assertEquals(2, Math.factorial(2));
-    // assertEquals(6, Math.factorial(3));
+    assertEquals(1, Math.factorial(0));
+    assertEquals(1, Math.factorial(1));
+    assertEquals(2, Math.factorial(2));
+    assertEquals(6, Math.factorial(3));
     assertEquals(24, Math.factorial(4));
   }
 
